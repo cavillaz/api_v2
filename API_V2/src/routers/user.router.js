9aef65490d8b916b0e1c9de773e90fb4f7333e17
Camilo Villa <andresvillaz@gmail.com>
2024-06-21 16:42:04 -0500

    Adicion de validaciones en el usuario y la contraseña

--- API_V2/src/routers/user.router.js
+++ API_V2/src/routers/user.router.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { createUser, createUserFK, deleteUser, showUser, showUserId, updateUser } from "../controllers/user.controller.js";
+import UserController from "../controllers/user.controller.js";
+import UserScheme from "../schemes/user.schema.js";
+import userMiddleware from "../middlewares/user.middlewares.js";
 
-const router =Router();
-router.post('/user',createUser);
-router.post('/userFK',createUserFK);
-router.get('/user',showUser);
-router.get('/user/:id',showUserId);
-router.put('/user/:id',updateUser);
-router.delete('/user/:id',deleteUser);
+const router=Router();
 
+router.post('/user',userMiddleware(UserScheme.createUser), UserController.createUser);
+router.get('/user', UserController.showUser);
+router.get('/user',UserController.showUserId);
+router.put('/user',userMiddleware(UserScheme.createUser),UserController.updateUser);
+router.delete('/user',UserController.deleteUser);
+router.post('/user/login',UserController.loginUser);
 
 export default router;
